a3a973bec5ae3eef657edfc2d786b68042ea3b8a
notgurev <notgurev@gmail.com>
2020-04-27 04:41:10 +0300

    Требование ввода логина и пароля на старте клиента (пока только заполняются переменные)

--- src/main/java/se1_prog_lab/client/ClientApp.java
+++ src/main/java/se1_prog_lab/client/ClientApp.java
@@ -12,12 +12,15 @@ import se1_prog_lab.client.interfaces.ServerIO;
 import java.util.Scanner;
 
 import static se1_prog_lab.util.BetterStrings.coloredYellow;
+import static se1_prog_lab.util.ValidatingReader.readString;
 
 @Singleton
 public class ClientApp implements Client {
     private final Scanner consoleScanner;
     private final CommandRepository commandRepository;
     private final ServerIO serverIO;
+    private String login;
+    private String password;
 
     @Inject
     public ClientApp(Scanner consoleScanner, CommandRepository commandRepository, ServerIO serverIO) {
@@ -35,6 +38,9 @@ public class ClientApp implements Client {
     @Override
     public void start() {
         System.out.println(coloredYellow("Начало работы клиента"));
+
+        authorize();
+
         serverIO.open();
 
         while (true) {
@@ -47,5 +53,33 @@ public class ClientApp implements Client {
         }
     }
 
+    /*
+     Пока чисто липовый метод, надо придумать как реально передавать login+password на сервер
+     Можно возвращать и отправлять на сервер команду Register/Login или как-то так...
+     Плюс еще нужна валидация, то есть нужно знать ответ, то есть возможно что вообще нужно
+     это все делать как клиентскую часть команды, я хз...
+    */
+    private void authorize() {
+        System.out.println("Для работы с коллекцией зарегистрироваться/авторизоваться");
+
+        String input;
+        do {
+            System.out.printf("Введите %s для регистрации или %s для авторизации \n",
+                    coloredYellow("register"), coloredYellow("login"));
+            input = consoleScanner.nextLine();
+        } while (!(input.equals("login") || input.equals("register")));
+
+        switch (input) {
+            case "login":
+                login = readString(consoleScanner, "Введите ваш логин: ", false, null);
+                password = readString(consoleScanner, "Введите ваш пароль: ", false, null);
+                break;
+            case "register":
+                login = readString(consoleScanner, "Придумайте логин: ", false, 1);
+                password = readString(consoleScanner, "Придумайте пароль: ", false, 1);
+                break;
+        }
+
+    }
 
 }
