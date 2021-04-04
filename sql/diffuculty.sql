e62f92f5289d5faabfa39ecc3b6043241c304d53
notgurev <notgurev@gmail.com>
2021-04-04 21:07:33 +0300

    merge all sql queries into one

--- sql/diffuculty.sql
+++
@@ -1,9 +0,0 @@
--- Type: difficulty
-
--- DROP TYPE public.difficulty;
-
-CREATE TYPE public.difficulty AS ENUM
-    ('EASY', 'HARD', 'IMPOSSIBLE');
-
-ALTER TYPE public.difficulty
-    OWNER TO postgres;
